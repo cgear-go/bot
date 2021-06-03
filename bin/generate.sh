39a7afb72ae2768f71cbe7f0db2b727a5040aeee
Jonathan ARNAULT <>
2021-06-03 08:56:00 +0000

    [store] Add kv store interface

---
+++ bin/generate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#   Copyright 2020 Pokémon GO Nancy
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+if [ ! -f .makerc ]; then
+    echo "Please run this command from repository root directory."
+    exit 1
+fi
+
+export GOPATH=${HOME}/go
+
+go install github.com/golang/mock/mockgen
+PATH=${PATH}:${GOPATH}/bin go generate ./...
